0fb9b3dcf9e1f4f1b498c8070beecafbe97b06e1
luodong.ld <luodong.ld@bytedance.com>
2021-03-20 20:19:05 +0800

    .

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-
-
+import "./model"
+import "fmt"
 func main(){
     //fmt.Println("asadsa")
+	elevs := model.Skill{}
+    elevs.Ack = 10;
+    elevs.Categoty = "dsadasda"
+    fmt.Println(elevs)
 
 }
